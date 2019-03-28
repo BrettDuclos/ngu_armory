c10e86bb634f6d2dda4656286086b421db52c304
Artem Gainanov <Artem.Gainanov@jetbrains.com>
2019-03-28 17:55:34 +0700

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAGWhB365jY7hLqc30Dnhwe5LOEvsivv58',
+    authDomain: 'ng-fitness-tracker-a01d9.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-a01d9.firebaseio.com',
+    projectId: 'ng-fitness-tracker-a01d9',
+    storageBucket: 'ng-fitness-tracker-a01d9.appspot.com',
+    messagingSenderId: '370569302627',
+  },
 };
